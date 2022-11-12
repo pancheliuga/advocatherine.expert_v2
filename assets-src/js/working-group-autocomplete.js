91b084fb774c96ae4d7a9f3620dd0abb95b795bc
Pancheliuga <pancheliuga.o@gmail.com>
2022-11-12 17:14:44 +0200

    WIP

--- assets-src/js/working-group-autocomplete.js
+++
@@ -1,7 +0,0 @@
-import simpleAccessibleAutocomplete from './libraries-extensions/s24-autocomplete';
-
-if (document.documentElement.classList.contains('js')) {
-
-	simpleAccessibleAutocomplete('filter-wg');
-
-}
