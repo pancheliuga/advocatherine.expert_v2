ea519a944d1735389fe84d27288434bf3e3cb3c5
Pancheliuga <pancheliuga.o@gmail.com>
2022-11-14 15:41:21 +0200

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,23 @@
+// Transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = config => {
+
+      // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform('htmlmin', htmlMinTransform);
+  }
+
     config.addWatchTarget("./src/assets/styles/");
-    // eleventyConfig.addPassthroughCopy("./src/css/");
-    // eleventyConfig.addPassthroughCopy("./src/images/");
+
+    // Passthrough copy
+    // Set directories to pass through to the dist folder
+    config.addPassthroughCopy('./src/assets/images/');
+    config.addPassthroughCopy('./src/assets/fonts/');
+    config.addPassthroughCopy('./src/assets/svg/');
 
     return {
         markdownTemplateEngine: 'njk',
